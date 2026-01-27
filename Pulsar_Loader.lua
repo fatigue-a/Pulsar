600cd7f30df2d98480a999ce892f1524e712707b
fatigue-a <devinv2409@gmail.com>
2026-01-27 17:16:55 -0500

    Update Pulsar_Loader.lua

--- Pulsar_Loader.lua
+++ Pulsar_Loader.lua
@@ -1,3 +1,10 @@
+--[[
+Trying to load without doing Key System first will not work!
+Trying to load without doing Key System first will not work!
+Trying to load without doing Key System first will not work!
+Trying to load without doing Key System first will not work!
+]]
+
 local ids = {
     [121864768012064] = "https://raw.githubusercontent.com/fatigue-a/Pulsar/refs/heads/main/Games/Fish-It/Fish-it.Lua",
 }
